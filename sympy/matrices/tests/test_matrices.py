ddd8e5950edb19b6cbe43a0fcf5218019ebac4b1
S.Y. Lee <sylee957@gmail.com>
2018-09-17 23:58:45 +0900

    Added illegal keyword check

--- sympy/matrices/tests/test_matrices.py
+++ sympy/matrices/tests/test_matrices.py
@@ -1894,6 +1894,7 @@ def test_errors():
     M = Matrix(((1, 2, 3, 4), (5, 6, 7, 8), (9, 10, 11, 12), (13, 14, 15, 16)))
     raises(ValueError, lambda: M.det('method=LU_decomposition()'))
     raises(TypeError, lambda: M.det(method=M))
+    raises(TypeError, lambda: M.det(method=lambda x: x))
     V = Matrix([[10, 10, 10]])
     M = Matrix([[1, 2, 3], [2, 3, 4], [3, 4, 5]])
     raises(ValueError, lambda: M.row_insert(4.7, V))
