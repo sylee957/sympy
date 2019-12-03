442225569cd0ad3dd94a410753c1d8f7a2d5a0b0
S.Y. Lee <sylee957@gmail.com>
2019-12-03 16:03:49 +0900

    Basic framework for matrix shape

---
+++ sympy/matrices/expressions/shape.py
@@ -0,0 +1,56 @@
+from sympy.core.containers import Tuple
+from sympy.core.evaluate import global_evaluate
+from sympy.core.expr import Expr
+from sympy.core.numbers import Integer
+from sympy.core.decorators import _sympifyit
+from sympy.matrices.matrices import MatrixBase
+
+
+class MatrixRows(Expr):
+    @_sympifyit('mat')
+    def __new__(cls, mat, evaluate=global_evaluate[0]):
+        if not mat.is_Matrix:
+            raise TypeError("{} must be a matrix.".format(mat))
+
+        if not evaluate:
+            return Expr.__new__(cls, mat)
+
+        if isinstance(mat, MatrixBase):
+            return Integer(mat.rows)
+
+        ret = mat.rows
+        if not isinstance(ret, MatrixRows):
+            return ret
+
+        return Expr.__new__(cls, mat)
+
+
+class MatrixCols(Expr):
+    @_sympifyit('mat')
+    def __new__(cls, mat, evaluate=global_evaluate[0]):
+        if not mat.is_Matrix:
+            raise TypeError("{} must be a matrix.".format(mat))
+
+        if not evaluate:
+            return Expr.__new__(cls, mat)
+
+        if isinstance(mat, MatrixBase):
+            return Integer(mat.cols)
+
+        ret = mat.cols
+        if not isinstance(ret, MatrixCols):
+            return ret
+
+        return Expr.__new__(cls, mat)
+
+
+class MatrixShape(Tuple):
+    @_sympifyit('mat')
+    def __new__(cls, mat, evaluate=global_evaluate[0]):
+        if not mat.is_Matrix:
+            raise TypeError("{} must be a matrix.".format(mat))
+
+        if not evaluate:
+            return Tuple.__new__(cls, mat)
+
+        return Tuple(MatrixRows(mat), MatrixCols(mat))
