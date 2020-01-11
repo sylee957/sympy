68ec7e41cf3401f4b1cfda8451ccb9ee14cb7384
S.Y. Lee <sylee957@gmail.com>
2020-01-11 14:13:36 +0900

    Add shape and rewriting to MatrixSlice

--- sympy/matrices/expressions/minor.py
+++ sympy/matrices/expressions/minor.py
@@ -4,14 +4,28 @@ from .matexpr import MatrixExpr
 
 
 class MinorSubmatrix(MatrixExpr):
-    def __new__(cls, i, j):
+    def __new__(cls, m, i, j):
+        m = _sympify(m)
+        if m.is_Matrix is not True:
+            raise ValueError(f"{m} should be a matrix.")
+
         i, j = _sympify(i), _sympify(j)
         if i.is_integer is False or j.is_integer is False:
             raise ValueError(f"{i}, {j} should be integers.")
 
-        return super(MinorSubmatrix, cls).__new__(cls, i, j)
+        return super(MinorSubmatrix, cls).__new__(cls, m, i, j)
+
+    @property
+    def shape(self):
+        rows, cols = self.args[0].shape
+        return rows-1, cols-1
 
     def _eval_rewrite_as_MatrixSlice(self, *args, **kwargs):
+        from .slice import MatrixSlice
+
         rows, cols = self.shape
-        i, j = self.args
-        pass
+        m, i, j = self.args
+        if i == 0 and j == 0:
+            return MatrixSlice(m, (1, rows), (1, cols))
+        elif i == rows and j == cols:
+            return MatrixSlice(m, (0, rows-1), (0, cols-1))
